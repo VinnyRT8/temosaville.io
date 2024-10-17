5601fdbe925a1479108569288ae6d72bd8db261b
VinnyRT8 <108861156+VinnyRT8@users.noreply.github.com>
2024-10-17 20:27:25 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-const arrow = document.getElementById('draggable');
+/* const arrow = document.getElementById('draggable');
 const rulesContent = document.getElementById('rules-content');
 
 arrow.addEventListener('mousedown', () => {
     rulesContent.style.display = 'block';
     rulesContent.style.maxHeight = '100vh';
 });
+ */
